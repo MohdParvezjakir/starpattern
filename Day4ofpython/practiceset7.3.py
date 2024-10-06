da121e3eaf5703ad1e31d0e6f61cbc8b7bcf5515
Mohd Parvez <mohdparvez989713@gmail.com>
2024-10-07 02:03:05 +0530

    Delete Day4ofpython directory

--- Day4ofpython/practiceset7.3.py
+++
@@ -1,7 +0,0 @@
-#write a program to calculate factorial of a given number using for loop
-n=int(input("Enter your number:"))
-product=1
-for i in range(1,(n +1)):
-    product=product *i
-
-print(f"the fectorial of{n} is {product}")
--- Day4ofpython/practiceset7.5.py
+++
@@ -1,5 +0,0 @@
-#write a program to print reverse table
-n=int(input("Enter your number:"))
-for i in range(1,11):
-    print(f"{n} x {11 -i} = {n*(11 -i)}")
-    
